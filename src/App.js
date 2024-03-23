1cb5e655678c2d5aa509684e08fc1a5494e00b12
JuYoung <rlaguswls9838@icloud.com>
2024-03-24 02:35:21 +0900

    modified api  url error

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
 
 	const getWeatherByCity = async () => {
 		try {
-			let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+			let url = `https://api.openweathermap.org/data/2.5/weather?q=${cities}&appid=${apiKey}&units=metric`
 			let response = await fetch(url)
 			let data = await response.json()
 			setWeather(data)
